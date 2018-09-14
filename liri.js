057f69cc64e91e104c0d69c7842b82ad4c6dea77
sydneycode <sstaggs12@gmail.com>
2018-09-13 19:53:00 -0700

    created a function to log the results to a file in addition to logging results to the console

--- liri.js
+++ liri.js
@@ -10,30 +10,30 @@ var keys = require('./keys.js');
 // Access your keys' information
 var spotify = new Spotify(keys.spotify);
 
+// Require the request, moment, and fs packages
 var request = require("request");
 var moment = require("moment");
 var fs = require("fs");
 
+// Store the array of command-line arguments in the args variable
 var args = process.argv;
-var command;
 
+var command;
 if (args.length > 2) {
     command = args[2];
-}
-else {
-    console.log("No command provided to LIRI.");
 }
 
 // Remove first 3 arguments from the array of command-line arguments, 
 // then join the remaining arguments with a space in between each pair of arguments
 var commandlineinput = args.slice(3).join(" ");
 
+var liriCommand = args.slice(2).join(" ");
+
 function getInfo(instruction, input) {
+    var dataToLog = "";
     switch(instruction) {
         case 'concert-this':
             //console.log("concert");
-            // var request = require("request");
-            // var moment = require("moment");
             var artist = input;
             var bandsInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
             request(bandsInTownURL, function(error, response, body) {
@@ -41,18 +41,23 @@ function getInfo(instruction, input) {
                     var output = JSON.parse(body);
                     for (var i = 0; i < output.length; i++) {
                         //console.log(output[i]);
-                        console.log("Name of the venue: " + output[i].venue.name);
+                        dataToLog += "Name of the venue: " + output[i].venue.name + "\r\n";
                         var region = output[i].venue.region;
                         if (region != '') {
                             region = region + ", ";
                         }
-                        console.log("Location of venue: " + output[i].venue.city + ", " + region + 
-                            output[i].venue.country);
+                        dataToLog += "Location of venue: " + output[i].venue.city + ", " + region + 
+                            output[i].venue.country + "\r\n";
                         var date = moment(output[i].datetime).format("MM/DD/YYYY");
-                        console.log("Date of the event: " + date + "\r\n");
+                        dataToLog += "Date of the event: " + date + "\r\n\r\n"; 
                     }
+
+                    // Log the data to the console and write it to the log file
+                    console.log(dataToLog);
+                    writeInfoToFile(liriCommand + "\r\n" + dataToLog);
                 }
             });
+            
             break;
 
         case 'spotify-this-song':
@@ -63,8 +68,8 @@ function getInfo(instruction, input) {
             if (input != "") {
                 song = input;
             }
-            spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
             //spotify.search({ type: 'track', query: song}, function(err, data) {
+            spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
                 if ( err ) {
                     console.log('Error occurred: ' + err);
                     return;
@@ -77,10 +82,15 @@ function getInfo(instruction, input) {
                 //     console.log("Preview link of the song from Spotify: " + results[i].preview_url);
                 //     console.log("Album that the song is from: " + results[i].album.name);
                 // }
-                console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name);
-                console.log("Name of the song: " + data.tracks.items[0].name);
-                console.log("Preview link of the song from Spotify: " + data.tracks.items[0].preview_url);
-                console.log("Album that the song is from: " + data.tracks.items[0].album.name + "\r\n");
+                // Get the necessary data for one song
+                dataToLog += "Artist(s): " + data.tracks.items[0].album.artists[0].name + "\r\n";
+                dataToLog += "Name of the song: " + data.tracks.items[0].name + "\r\n";
+                dataToLog += "Preview link of the song from Spotify: " + data.tracks.items[0].preview_url + "\r\n";
+                dataToLog += "Album that the song is from: " + data.tracks.items[0].album.name + "\r\n\r\n";
+
+                // Log the data to the console and write it to the log file
+                console.log(dataToLog);
+                writeInfoToFile(liriCommand + "\r\n" + dataToLog);
             });
             break;
 
@@ -97,14 +107,19 @@ function getInfo(instruction, input) {
                 if (!error && response.statusCode === 200) {
                     var output = JSON.parse(body);
                     //console.log(output);
-                    console.log("Title of the movie: " + output.Title);
-                    console.log("Year the movie came out: " + output.Year);
-                    console.log("IMDB Rating of the movie: " + output.Ratings[0]['Value']);
-                    console.log("Rotten Tomatoes Rating of the movie: " + output.Ratings[1]['Value']);
-                    console.log("Country where the movie was produced: " + output.Country);
-                    console.log("Language of the movie: " + output.Language);
-                    console.log("Plot of the movie: " + output.Plot);
-                    console.log("Actors in the movie: " + output.Actors + "\r\n");
+                    dataToLog =
+                        "Title of the movie: " + output.Title + "\r\n" +
+                        "Year the movie came out: " + output.Year + "\r\n" +
+                        "IMDB Rating of the movie: " + output.Ratings[0]['Value'] + "\r\n" +
+                        "Rotten Tomatoes Rating of the movie: " + output.Ratings[1]['Value'] + "\r\n" +
+                        "Country where the movie was produced: " + output.Country + "\r\n" +
+                        "Language of the movie: " + output.Language + "\r\n" +
+                        "Plot of the movie: " + output.Plot + "\r\n" +
+                        "Actors in the movie: " + output.Actors + "\r\n\r\n";
+                    
+                    // Log the data to the console and write it to the log file
+                    console.log(dataToLog);
+                    writeInfoToFile(liriCommand + "\r\n" + dataToLog);
                 }
             });
             break;
@@ -126,9 +141,22 @@ function getInfo(instruction, input) {
             break;
 
         default:
-            console.log("default");
+            //console.log("default");
+            console.log("No command provided to LIRI");
             break;
     }
+}
+
+function writeInfoToFile(info) {
+    fs.appendFile("log.txt", info, function(err) {
+        // Log any errors to the console
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log("Content added to log.txt");
+        }
+    });
 }
 
 getInfo(command, commandlineinput);
